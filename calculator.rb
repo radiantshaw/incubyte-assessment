02fec14b7a2199962795eddf03e956ba4d7d025f
Nipun Paradkar <nipunparadkar123@gmail.com>
2024-07-24 19:18:16 +0530

    Add comma separated numbers

---
+++ calculator.rb
@@ -0,0 +1,20 @@
+class Calculator
+  def add(string_of_numbers)
+    string_of_numbers
+      .split(",")
+      .map(&:to_i)
+      .reduce do |memo, number|
+        memo + number
+      end
+  end
+end
+
+RSpec.describe Calculator do
+  let(:calculator) { described_class.new }
+
+  describe "#add" do
+    it "adds numbers separated by comma" do
+      expect(calculator.add("1,5")).to eq(6)
+    end
+  end
+end
